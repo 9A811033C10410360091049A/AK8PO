833096fdaf409d4e70a94a3d4641a638df888156
9A811033C10410360091049A <bougi23@seznam.cz>
2023-07-15 10:39:11 +0200

    Adding Finish functionality skeleton

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;
+        renderer = GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        // TODO implement finish
+        if(other.gameObject.tag == "Player") 
+        {
+            Debug.Log("Player hit the finish - respawning back to initial position");
+            //GameObject players = GameObject.FindGameObjectsWithTag ("Player");
+            //other.gameObject.position = startPosition;
+            //player.position = startPosition;
+        //foreach (GameObject player in players)
+        //{
+        //    Pos = player.transform.startPosition;
+        //}
+
+        }
     }
 }
